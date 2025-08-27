26dccadfc97a6df46ea7673e17047d499a42ce6a
aswanivaChaithram <aswaniva040@gmail.com>
2025-08-27 11:58:55 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Food-Delivery-Website/',
   server: {
     open: true,        // auto open in browser on start
   },
