27c262e470914ee316950b83634eeded270f0eb2
aswanivaChaithram <aswaniva040@gmail.com>
2025-09-02 13:38:53 +0530

    add logic to store data in DB

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,7 +3,23 @@ import fs from 'fs';
 
 // add food items 
 const addFood = async (req, res) => {
+    let image_filename = `${req.file.filename}`;
 
+    const food = new foodModel({
+        name: req.body.name,
+        description: req.body.description,
+        price: req.body.price,
+        image: image_filename,
+        category: req.body.category
+    })
+
+    try {
+        await food.save();
+        res.json({ success: true, message: "Food item added successfully" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error adding food item" });
+    }
 }
 
 export { addFood };
